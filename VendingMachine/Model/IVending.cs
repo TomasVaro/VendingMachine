404fce8d9b12696923af0aee63fc23dad904faf0
tomasvaro <tomas.varo63@gmail.com>
2021-10-20 07:47:10 +0200

    Added interface to VendingMachine

---
+++ VendingMachine/Model/IVending.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine.Model
+{
+    public interface IVending
+    {
+        void Purchase();
+        void ShowAll();
+        void InserMoney();
+        void EndTransaction();
+    }
+}
---
+++ VendingMachine/Model/VendingMachine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingMachine.Model
+{
+    class VendingMachine : IVending
+    {
+        public string Product { get; set; }
+        public int Price { get; set; }
+        public string Message { get; set; }
+
+        public VendingMachine(string product, int price, string message)
+        {
+            Product = product;
+            Price = price;
+            Message = message;
+        }
+        public void Purchase()
+        {
+            //To buy a product
+        }
+        public void ShowAll()
+        {
+            //Show all products
+        }
+        public void InserMoney()
+        {
+            //Add money to MoneyPool
+        }
+        public void EndTransaction()
+        {
+            //Returns money left in appropriate amount of change
+        }
+    }
+}
